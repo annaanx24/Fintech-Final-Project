177208c2b0b197749e5d24e9b5d5372a9f236b61
annaanx24 <annaxia24@gmail.com>
2020-07-29 17:41:00 +0000

    updated coin game

--- static/js/coinGame.js
+++ static/js/coinGame.js
@@ -3,7 +3,7 @@
 /* global dist, angleMode, DEGREES, arc, clear, createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, rectMode, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, tint, noTint
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, noFill, windowWidth, windowHeight, noStroke, 
-          key, keyCode, createGraphics, textAlign, createButton, CENTER, BOLD, LEFT,TOP,textStyle, PI, HALF_PI, Color, UP_ARROW, round, millis, keyIsDown, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
+          key, keyCode, createGraphics, textAlign, createButton, CENTER, CORNER, BOLD, LEFT,TOP,textStyle, PI, HALF_PI, Color, UP_ARROW, round, millis, keyIsDown, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
 // Collide2D functions:
 /* global collideRectCircle */
@@ -33,11 +33,12 @@ let timeLimit;
 let popup;
 let mode;
 let selectedItem;
-let ipadImg 
-let poolImg 
-let legoImg 
-let candyImg 
-
+let ipadImg;
+let poolImg;
+let legoImg;
+let candyImg;
+let prizes;
+let collection;
 
 class Coin {
   constructor(x, y, d, fallSpeed) {
@@ -100,13 +101,14 @@ class Pig {
 }
 
 function setup() {
-  createCanvas(500, 500);
+  let canvas = createCanvas(500, 500);
+//   canvas.parent('coindash-holder');
   colorMode(HSB);
-  score = 0;
+  score = 50;
   mode = 1;
-  timeLimit = 5;
-  selectedItem = ""
-  popup = createGraphics(300, 300);
+  timeLimit = 3;
+  selectedItem = "";
+  popup = createGraphics(320, 100)
   images();
   coins = [];
   for (let i = 0; i < 3; i++) {
@@ -115,69 +117,68 @@ function setup() {
   // Variables for droplet 2
   pig = new Pig();
   time = 0;
-
   ipad = new Prize(30, 45, "iPad", 400);
-  pool = new Prize(270, 45, "Pool", 250);
-  lego = new Prize(30, 270, "Legos", 50);
-  candy = new Prize(270, 270, "Candy", 10);
-
-  
+  pool = new Prize(270, 45, "pool", 250);
+  lego = new Prize(30, 270, "legos", 50);
+  candy = new Prize(270, 270, "candy", 10);
+  prizes = [ipad, pool, lego, candy];
+  collection = [];
 }
 
 function draw() {
   background(195, 27, 97);
-  
+
   //move all the coins
   if (mode == 1) {
     screen1();
-  }
-  else if (mode ==2) {
+  } else if (mode == 2) {
     screen2();
-  }
-  else if(mode ==3) {
+  } else if (mode == 3) {
     screen3();
+  } else if (mode == 4) {
+    screen4();
   }
 }
 
 function screen1() {
   if (!isEndGame) {
-      for (let i = 0; i < coins.length; i++) {
-        coins[i].y += coins[i].fallSpeed;
-        // If it goes off the screen...
-        if (coins[i].y > height) {
-          // ...reset it...
-          coins[i].y = 0;
-          // ...and move it somewhere random.
-          coins[i].x = random(width);
-        }
-        coins[i].display();
+    for (let i = 0; i < coins.length; i++) {
+      coins[i].y += coins[i].fallSpeed;
+      // If it goes off the screen...
+      if (coins[i].y > height) {
+        // ...reset it...
+        coins[i].y = 0;
+        // ...and move it somewhere random.
+        coins[i].x = random(width);
       }
-
-      overlayImages();
-
-      pig.move();
-      pig.checkCollection();
-      textSize(20);
-      textStyle(BOLD);
-      textAlign(LEFT, TOP);
-      fill(144, 100, 63);
-      text("Coins Collected: " + score, 10, 30);
-      time = round((millis() - resetTime) / 1000);
-      text("Time left: " + (timeLimit - time), 10, 60);
+      coins[i].display();
     }
 
-    if (time > timeLimit) {
-      mode = 2
-    }
+    overlayImages();
+
+    pig.move();
+    pig.checkCollection();
+    textSize(20);
+    textStyle(BOLD);
+    textAlign(LEFT, TOP);
+    fill(144, 100, 63);
+    text("Coins Collected: " + score, 10, 30);
+    time = round((millis() - resetTime) / 1000);
+    text("Time left: " + (timeLimit - time), 10, 60);
+  }
+
+  if (time > timeLimit) {
+    mode = 2;
+  }
 }
 
 function screen3() {
-  ipad.display();
-  pool.display();
-  lego.display();
-  candy.display();
-  textSize (15)
-  text(`You have ${score} coins`, width/2, 20)
+  ipad.displayPrize();
+  pool.displayPrize();
+  lego.displayPrize();
+  candy.displayPrize();
+  textSize(15);
+  text(`You have ${score} coins`, width / 2, 20);
   // text(mouseX, 10, 10)
   // text(mouseY, 10, 20)
   //display item images
@@ -185,120 +186,150 @@ function screen3() {
   image(poolImg, pool.x, pool.y, pool.size, pool.size);
   image(legoImg, lego.x, lego.y, lego.size, lego.size);
   image(candyImg, candy.x, candy.y, candy.size, candy.size);
-  
+
   //to display popup of if the purchase is possible
-  popup.fill(255,255,255)
-  popup.textAlign(LEFT)
-  if (selectedItem=="ipad") {
-    
-    popup.clear()
-    popup.background('rgba(0, 0, 0, 0.75)')
-    
-    
+  popup.fill(255, 255, 255);
+  popup.textAlign(LEFT);
+  if (selectedItem == "iPad") {
+    popup.clear();
+    popup.background("rgba(0, 0, 0, 0.60)");
+
     if (ipad.checkPurchase()) {
-      console.log("enough money")
-      popup.text(`You have enough coins to buy the ${selectedItem}!\n Press v to buy!`,40,40)
-       
+      popup.text(
+        `You have enough coins to buy the ${selectedItem}!\n Press v to buy!`,
+        40,
+        40
+      );
+    } else {
+      popup.text(
+        `Sorry, you do not have enough coins to buy\nthe ${selectedItem}. Click next to continue\ncollecting coins!`,
+        40,
+        40
+      );
     }
-    else {
-      console.log("NOT enough money")
-      popup.text(`Sorry, you do not have enough coins to buy\nthe ${selectedItem}. Press click another item to close.`, 40,40)
-      
-    }
-    image(popup, 50,50)
     
-  }  
-  else if (selectedItem=="pool") {
-    popup.clear()
-    console.log("pool")
-     popup.background('rgba(0, 0, 0, 0.75)')
-    if (pool.checkPurchase()) {
-      console.log("enough money")
-      popup.text(`You have enough coins to buy the ${selectedItem}.\nBut you are ${ipad.price-score} coins from buying an ipad! \nPress v to buy ${selectedItem}. Click next to continue\ncollecting coins!`,10,40)
-     
+  } else if (selectedItem == "pool") {
+    popup.clear();
+
+    popup.background("rgba(0, 0, 0, 0.75)");
+    if (score >= ipad.price) {
+      popup.text(
+        `You have enough coins to buy the ${selectedItem}!\n Press v to buy!`,
+        40,
+        40
+      );
+    } else if (pool.checkPurchase()) {
+      popup.text(
+        `You have enough coins to buy the ${selectedItem}.\nBut you are ${ipad.price -
+          score} coins from buying an ipad! \nPress v to buy ${selectedItem}. Click next to continue\ncollecting coins!`,
+        10,
+        40
+      );
+    } else {
+      popup.text(
+        `Sorry, you do not have enough coins to buy \nthe ${selectedItem}. Click next to continue collecting coins!`,
+        40,
+        40
+      );
     }
-    else {
-      console.log("NOT enough money")
-      popup.text(`Sorry, you do not have enough coins to buy \nthe ${selectedItem}. Click another item to close.\nClick next to continue collecting coins!`, 40,40)
-      
+  } else if (selectedItem == "legos") {
+    popup.clear();
+    popup.background("rgba(0, 0, 0, 0.75)");
+
+    if (score >= pool.price) {
+      popup.text(
+        `You have enough coins to buy the ${selectedItem}!\n Press v to buy!`,
+        40,
+        40
+      );
+    } else if (lego.checkPurchase()) {
+      popup.text(
+        `You have enough coins to buy the ${selectedItem}.\nBut you are ${pool.price -
+          score} coins from buying a pool! \nPress v to buy ${selectedItem}. Click next to continue\ncollecting coins!`,
+        40,
+        40
+      );
+    } else {
+      popup.text(
+        `Sorry, you do not have enough coins to buy \n${selectedItem}. Click next to continue collecting coins!`,
+        40,
+        40
+      );
     }
-    image(popup, 50,50)
-  }  
-  
-  else if (selectedItem=="lego") {
-    popup.clear()
-     popup.background('rgba(0, 0, 0, 0.75)')
-    console.log("lego")
-    if (lego.checkPurchase()) {
-      console.log("enough money")
-      popup.text(`You have enough coins to buy the ${selectedItem}s.\nBut you are ${pool.price-score} coins from buying a pool! Press v to buy ${selectedItem}. Click next to continue\ncollecting coins!`,40,40)
-     
+  } else if (selectedItem == "candy") {
+    popup.clear();
+    popup.background("rgba(0, 0, 0, 0.75)");
+
+    if (score >= lego.price) {
+      popup.text(
+        `You have enough coins to buy the ${selectedItem}!\n Press v to buy!`,
+        40,
+        40
+      );
+    } else if (candy.checkPurchase()) {
+      popup.text(
+        `You have enough coins to buy ${selectedItem}.\nBut you are ${lego.price -
+          score} coins from buying legos!\nPress v to buy ${selectedItem}. Click next to continue\ncollecting coins!`,
+        40,
+        40
+      );
+    } else {
+      popup.text(
+        `Sorry, you do not have enough coins to buy \n the ${selectedItem}.\n Click next to continue collecting coins!`,
+        40,
+        40
+      );
     }
-    else {
-      console.log("NOT enough money")
-      popup.text(`Sorry, you do not have enough coins to buy \n${selectedItem}s. Click another item to close.\nClick next to continue collecting coins!`, 40,40)
-      
-    }
-    image(popup, 50,50)
-  }  
-  else if (selectedItem=="candy") {
-    popup.clear()
-    popup.background('rgba(0, 0, 0, 0.75)')
-    console.log("candy")
-    if (candy.checkPurchase()) {
-      console.log("enough money")
-      popup.text(`You have enough coins to buy ${selectedItem}.\nBut you are ${lego.price-score} coins from buying legos! Press v to buy ${selectedItem}. Click next to continue\ncollecting coins!`,40,40)
-     
-    }
-    else {
-      console.log("NOT enough money")
-      popup.text(`Sorry, you do not have enough coins to buy \n the ${selectedItem}. Click another item to close. \n Click next to continue collecting coins!`, 40,40)
-      
-    }
-    image(popup, 50,50)
-  }  
+    
+  }
+  // rectMode(CENTER)
+  image(popup, width/2-160, height/2-50);
+  // rectMode(CORNER)
 }
 
 function mouseClicked() {
-  if (mouseX<230&&mouseX>30&&mouseY>45&&mouseY<245) {
-   console.log("ipad")
-    selectedItem = "ipad"
-  }  
-  else if (mouseX<470&&mouseX>270&&mouseY>45&&mouseY<245) {
-    console.log("pool")
-    selectedItem = "pool"
-  }  
-  
-  else if (mouseX<230&&mouseX>30&&mouseY>280&&mouseY<480) {
-    console.log("lego")
-    selectedItem = "lego"
-  }  
-  else if (mouseX<470&&mouseX>270&&mouseY>280&&mouseY<480) {
-    console.log("candy")
-    selectedItem = "candy"
-  }  
-  
+  if (mouseX < 230 && mouseX > 30 && mouseY > 45 && mouseY < 245) {
+    selectedItem = "iPad";
+  } else if (mouseX < 470 && mouseX > 270 && mouseY > 45 && mouseY < 245) {
+    selectedItem = "pool";
+  } else if (mouseX < 230 && mouseX > 30 && mouseY > 280 && mouseY < 480) {
+    selectedItem = "legos";
+  } else if (mouseX < 470 && mouseX > 270 && mouseY > 280 && mouseY < 480) {
+    selectedItem = "candy";
+  }
 }
 
- 
 function screen2() {
- 
-    let coin = "coins";
-    if (score == 1) {
-      coin = "coin";
-    }
-    textSize(40);
-    text(
-      "Game Over! \n Congratulations! \n You collected " + score + " " + coin+ ". \n Click the next button \n to buy prizes!",
-      250,
-      250
-    );
-    textAlign(CENTER, CENTER);
+  let coin = "coins";
+  if (score == 1) {
+    coin = "coin";
+  }
+  textSize(40);
+  text(
+    "Game Over! \n Congratulations! \n You collected " +
+      score +
+      " " +
+      coin +
+      ". \n Click the next button \n to buy prizes!",
+    250,
+    250
+  );
+  textAlign(CENTER, CENTER);
 
-    button2 = createButton("Next!");
-    button2.position(width - 50, 19);
-    button2.mousePressed(prizeSelection);
-
+  button2 = createButton("Next!");
+  button2.position(width - 50, 19);
+  button2.mousePressed(prizeSelection);
+}
+function screen4() {
+  text(`Your prize collection!`, width / 2, 20);
+  image(ipadImg, ipad.x, ipad.y, ipad.size, ipad.size);
+  image(poolImg, pool.x, pool.y, pool.size, pool.size);
+  image(legoImg, lego.x, lego.y, lego.size, lego.size);
+  image(candyImg, candy.x, candy.y, candy.size, candy.size);
+  ipad.displayCollection();
+  pool.displayCollection();
+  lego.displayCollection();
+  candy.displayCollection();
 }
 
 class Prize {
@@ -307,43 +338,64 @@ class Prize {
     this.y = y;
     this.name = name;
     this.price = price;
-    this.image = 10; //change this var to actual image when you get it
+    this.image = 10; //change this var
     this.size = 200;
+    this.count = 0;
   }
 
-  display() {
-    
-    text(`${this.name} | ${this.price} coins`, this.x+100, this.y+210)
+  displayPrize() {
+    let tempName = this.name;
+    if (this.name != "iPad") {
+      tempName = this.name.replace(/^\w/, c => c.toUpperCase());
+    }
+    text(`${tempName} | ${this.price} coins`, this.x + 100, this.y + 210);
     // text(this.price +" coins", this.x+100, this.y+225)
   }
-  
-  checkPurchase() {
-    if (score>=this.price) {
-      // popup.text("you can make purchase")
-      // image(popup, 50, 50)
-      return true
-      console.log("checking purchase")
+
+  displayCollection() {
+    if (this.name == "legos") {
+      text(`You have ${this.count} ${this.name}!`, this.x + 100, this.y + 210);
+    } 
+    else if (this.name == "candy") {
+      text(`You have ${this.count} candies!`, this.x + 100, this.y + 210);
     }
-    else 
-      // popup.text("you can't make purchase")
-      // image(popup, 50, 50)
-      console.log("not enough money")
-      return false
+    else {
+      text(`You have ${this.count} ${this.name}s!`, this.x + 100, this.y + 210);
+    }
+  }
+
+  checkPurchase() {
+    if (score >= this.price) {
+      return true;
+    } else return false;
+  }
+}
+function keyPressed() {
+  if (mode == 3 && key == "v") {
+    console.log("key v pressed");
+    buy(selectedItem);
   }
 }
 
+function buy(item) {
+  console.log("in buy function");
+  for (let i = 0; i < prizes.length; i++) {
+    if (item == prizes[i].name) {
+      if (prizes[i].checkPurchase()) {
+        score -= prizes[i].price;
+        prizes[i].count += 1;
+        console.log("bought" + prizes[i].name);
+      }
+    }
+  }
+}
 function prizeSelection() {
-  if (mode!=3) {
-    mode+=1
+  if (mode != 4) {
+    mode += 1;
+  } else {
+    mode = 1;
+    restart();
   }
-  else {
-    mode = 1
-    restart()
-  }
-
-  // button = createButton("Restart");
-  // button.position(width - 80, 19);
-  // button.mousePressed(restart);
 }
 
 function images() {
@@ -354,14 +406,22 @@ function images() {
   coinImg = loadImage(
     "https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2F25498.jpg?v=1595864737068"
   );
-  
-  ipadImg = loadImage("https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2Fipad%20image.jpg?v=1595991487563")
-  
-  poolImg = loadImage("https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2Fpool%20image.png?v=1595991705340")
-  
-  legoImg = loadImage("https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2Flego%20man.jpg?v=1595991708603")
-  
-  candyImg = loadImage("https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2Fcandy.jpg?v=1595991715711")
+
+  ipadImg = loadImage(
+    "https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2Fipad%20image.jpg?v=1595991487563"
+  );
+
+  poolImg = loadImage(
+    "https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2Fpool%20image.png?v=1595991705340"
+  );
+
+  legoImg = loadImage(
+    "https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2Flego%20man.jpg?v=1595991708603"
+  );
+
+  candyImg = loadImage(
+    "https://cdn.glitch.com/075a41f8-cc68-4f5f-af51-36b05eee518b%2Fcandy.jpg?v=1595991715711"
+  );
 }
 
 function overlayImages() {
